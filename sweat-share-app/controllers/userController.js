10c7e577f22a0b8e3d19ede56ef2066f3393d67e
psantos2107 <paul.santos2107@gmail.com>
2024-03-06 21:56:08 -0800

    created user schema, worked on sessions controller, user controller

--- sweat-share-app/controllers/userController.js
+++ sweat-share-app/controllers/userController.js
@@ -1,7 +1,8 @@
+const bcrypt = require("bcrypt");
 const User = require("./../models/user");
 
 const newUser = (req, res) => {
-  res.send("user new");
+  res.render("/userViews/new.ejs");
 };
 
 const deleteUser = (req, res) => {
@@ -12,16 +13,33 @@ const updateUser = (req, res) => {
   res.send("user patch");
 };
 
-const createUser = (req, res) => {
-  res.send("user create");
+const createUser = async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(
+      req.body.password,
+      await bcrypt.genSalt(10)
+    );
+    const newUser = await User.create(req.body);
+    req.session.currentUser = newUser;
+    console.log(newUser);
+    console.log(req.session.currentUser);
+    res.redirect(`/users/${newUser._id}`);
+  } catch (err) {
+    res.render("error.ejs", { error: err.message });
+  }
 };
 
 const editUser = (req, res) => {
   res.send("user edit");
 };
 
-const showUser = (req, res) => {
-  res.send("user show");
+const showUser = async (req, res) => {
+  try {
+    const user = await db.findById(req.params.id);
+    res.render("/userViews/show.ejs", { user });
+  } catch {
+    res.render("error.ejs", { error: "User not found or does not exist." });
+  }
 };
 
 module.exports = {
---
+++ sweat-share-app/models/user.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    minLength: [1, "Your username must contain at least one character."],
+  },
+  password: { type: String, required: true, trim: true },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [1, "Your name must contain at least one character."],
+  },
+  age: { type: Number },
+  location: { type: String },
+  profilePic: {
+    type: String,
+    default: path.join(
+      __dirname,
+      "..",
+      "..",
+      "public",
+      "imgs",
+      "emptyProfilePic.jpg"
+    ),
+  },
+  faveExercise: { type: String },
+  about: { type: String, trim: true },
+  createdAt: { type: Date, default: Date.now(), select: false },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
